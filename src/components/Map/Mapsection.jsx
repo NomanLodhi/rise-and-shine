52554fd06af775619e5def3be18ce540fc81ddb9
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2025-07-08 16:29:46 -0700

    Chnages

--- src/components/Map/Mapsection.jsx
+++ src/components/Map/Mapsection.jsx
@@ -3,6 +3,23 @@ import Map from './Map'
 import SectionHeading from '../SectionHeading/SectionHeading'
 
 export default function Mapsection() {
+    const serviceAreas = [
+  "Langley",
+  "Pitt Meadows",
+  "Maple Ridge",
+  "Aldergrove",
+  "White Rock",
+  "Surrey",
+  "New West",
+  "North Vancouver",
+  "Burnaby",
+  "Richmond",
+  "Delta",
+  "West Vancouver",
+  "East Vancouver",
+  "Chilliwack"
+];
+
   return (
     <div>
          <div className="ak-height-125 ak-height-lg-80"></div>
@@ -10,6 +27,13 @@ export default function Mapsection() {
                 <SectionHeading
                            title={"Areas we covered"}
                            />
+            <ul className="cities-list">
+                {
+                    serviceAreas.map((city)=>(
+                        <li>{city}</li>
+                    ))
+                }
+            </ul>
             </div>
                 <p className='text-start p-1' style={{fontSize:'13px'}}> *Travelling Charges may apply for the distance more than 20kms</p>           
       <Map/>
